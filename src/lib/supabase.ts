0f49cbb81a59b316edd295d05fabfbbd296c23e6
Sergio Castro <sergiojuniorce@gmail.com>
2022-12-16 00:14:19 -0700

    Removed pnpm lock

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  PUBLIC_SUPABASE_URL,
-  PUBLIC_SUPABASE_KEY
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 )
