c996db5a8dad26a36de7ca066244e5b668367e03
Sergio Castro <sergiojuniorce@gmail.com>
2022-12-16 00:13:43 -0700

    Removed pnpm

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
-
+import { createClient } from '@supabase/supabase-js';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_KEY
 )
