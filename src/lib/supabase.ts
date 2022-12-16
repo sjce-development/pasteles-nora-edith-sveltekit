fde2b3408cb8a0cece4dbf003518e17c2a54d9af
Sergio Castro <sergiojuniorce@gmail.com>
2022-12-16 00:15:37 -0700

    Fix?

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,4 @@ import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
-)
+);
