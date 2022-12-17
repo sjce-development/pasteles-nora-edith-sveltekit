64b59ea76c94c464acf915d4328fcca23bfc9a06
Sergio Castro <sergiojuniorce@gmail.com>
2022-12-17 16:04:20 -0700

    reverted supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_KEY } from '$env/static/public';
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
